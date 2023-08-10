be531c9a2c9eb9a7f4cd7d53e66b1cb41cded307
ranjeetkings <85012660+ranjeetkings@users.noreply.github.com>
2023-08-10 12:47:00 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular';
+
+  input01: any;
+  input02: any;
+  input03: any;
+  input04: any;
+  formInput: any;
+
+  validatePin1() {
+    this.formInput = this.input01 + this.input02 + this.input03 + this.input04;
+  }
 }
