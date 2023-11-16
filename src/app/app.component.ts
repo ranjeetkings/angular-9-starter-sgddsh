69300ddfb7e80199aaf0984a666b8522603c3b90
ranjeetkings <85012660+ranjeetkings@users.noreply.github.com>
2023-11-16 18:28:30 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,9 +12,15 @@ export class AppComponent {
   input02: any;
   input03: any;
   input04: any;
+  inpLen: any = false;
   formInput: any;
 
+  updateButtonState(val) {
+    this.inpLen = val.length < 4 ? false : true;
+  }
+
   validatePin1() {
-    this.formInput = this.input01 + this.input02 + this.input03 + this.input04;
+    // this.formInput = this.input01 + this.input02 + this.input03 + this.input04;
+    this.formInput = this.input01;
   }
 }
